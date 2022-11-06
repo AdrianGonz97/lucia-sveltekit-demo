104494deb97e9fb0b41d54bcdf79a3f78be41aad
AdrianGonz97 <31664583+AdrianGonz97@users.noreply.github.com>
2022-11-06 16:08:56 -0500

    prisma

--- src/app.d.ts
+++ src/app.d.ts
@@ -2,8 +2,21 @@
 // for information about these interfaces
 // and what to do when importing types
 declare namespace App {
-	// interface Locals {}
+	interface Locals {
+		getSession: import('@lucia-auth/sveltekit').GetSession;
+		getSessionUser: import('@lucia-auth/sveltekit').GetSessionUser;
+		setSession: import('@lucia-auth/sveltekit').SetSession;
+		clearSession: import('@lucia-auth/sveltekit').ClearSession;
+	}
 	// interface PageData {}
 	// interface Error {}
 	// interface Platform {}
+}
+
+/// <reference types="lucia-auth" />
+declare namespace Lucia {
+	type Auth = import('$lib/server/lucia').Auth;
+	type UserAttributes = {
+		// username: string;
+	};
 }
